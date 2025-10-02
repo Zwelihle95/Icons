41313b75a716918307b5dff002fade19d79eadb7
khlongwa-dev <ayandahlongwa21@gmail.com>
2025-10-02 20:27:25 +0200

    feat (domain/exceptions) : added an invalid Id number exception.

---
+++ ma-di-ventures-lms-backend/LMS.Domain/Exceptions/InvalidIdNumberException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMS.Domain.Exceptions
+{
+    public class InvalidIdNumberException : Exception
+    {
+        public InvalidIdNumberException(string message) : base(message) { }
+    }
+}
