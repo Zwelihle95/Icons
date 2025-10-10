73daf659023971d858c816a345cd756a8aa20093
khlongwa-dev <ayandahlongwa21@gmail.com>
2025-10-10 19:49:35 +0200

    feat (application/admin) : implemented a query to get all students.

--- ma-di-ventures-lms-backend/LMS.Application/Admins/Queries/GetAllStudents/GetAllStudentsQuery.cs
+++ ma-di-ventures-lms-backend/LMS.Application/Admins/Queries/GetAllStudents/GetAllStudentsQuery.cs
@@ -4,10 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LMS.Application.Admin.Queries.GetAllStudents
+namespace LMS.Application.Admins.Queries.GetAllStudents
 {
     public class GetAllStudentsQuery
     {
-
+        public string? CompanyFilter { get; set; }
     }
 }
--- ma-di-ventures-lms-backend/LMS.Application/Admins/Queries/GetAllStudents/GetAllStudentsQueryHandler.cs
+++ ma-di-ventures-lms-backend/LMS.Application/Admins/Queries/GetAllStudents/GetAllStudentsQueryHandler.cs
@@ -1,13 +1,40 @@
-﻿using System;
+﻿using LMS.Application.Students.DTOs;
+using LMS.Domain.Entities;
+using LMS.Domain.Repositories.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LMS.Application.Admin.Queries.GetAllStudents
+namespace LMS.Application.Admins.Queries.GetAllStudents
 {
     public class GetAllStudentsQueryHandler
     {
+        private readonly IUserRepository _userRepository;
 
+        public GetAllStudentsQueryHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<IEnumerable<StudentDto>> HandleAsync(GetAllStudentsQuery query)
+        {
+            var students = await _userRepository.GetAllByRoleAsync<Student>();
+
+            if (!string.IsNullOrEmpty(query.CompanyFilter))
+            {
+                students = students.Where(s => s.Company == query.CompanyFilter);
+            }
+
+            return students.Select(s => new StudentDto
+            {
+                Id = s.Id,
+                FirstName = s.FirstName,
+                LastName = s.LastName,
+                Company = s.Company,
+                Email = s.Email.UserEmail,
+            });
+        }
     }
 }
